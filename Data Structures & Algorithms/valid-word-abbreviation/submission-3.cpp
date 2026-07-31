1c4baded5d1f461faded5132d45c3a679e58d607
Tanmay Achare <152853922+Tanmayachare@users.noreply.github.com>
2026-07-31 11:33:22 +0530

    Add: valid-word-abbreviation - submission-3

---
+++ Data Structures & Algorithms/valid-word-abbreviation/submission-3.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool validWordAbbreviation(string word, string abbr) {
+        int l=0;
+        int r=0;
+        if(word == abbr) return true;
+        while(l<word.length() && r<abbr.length()){
+            if(isdigit(abbr[r])){
+                int nums = 0;
+                if(abbr[r]=='0') return false;
+                while(r<abbr.length() && isdigit(abbr[r])){
+                    nums = nums*10 + (abbr[r] - '0');
+                    r++;
+                }
+                l+=nums;
+            }
+            else{
+                if(word[l]==abbr[r]) {
+                    l++;
+                    r++;
+                }
+                else return false;
+            }
+        }
+        return l==word.length() && r==abbr.length();
+    }
+};
